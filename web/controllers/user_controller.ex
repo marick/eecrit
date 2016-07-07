69eac814be49090e72eb781610b6121910a0b475
Brian Marick <marick@exampler.com>
2016-07-07 14:53:20 -0500

    Chapter 4

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,5 +1,7 @@
 defmodule Eecrit.UserController do
   use Eecrit.Web, :controller
+  alias Eecrit.User
+  alias Eecrit.Repo
 
   def index(conn, _params) do
     users = Repo.all(Eecrit.User)
@@ -9,6 +11,23 @@ defmodule Eecrit.UserController do
   def show(conn, %{"id" => id}) do
     user = Repo.get(Eecrit.User, String.to_integer(id))
     render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} -> 
+        conn
+        |> put_flash(:info, "#{user.display_name} created.")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
 
--- web/models/user.ex
+++ web/models/user.ex
@@ -1,6 +1,8 @@
 defmodule Eecrit.User do
   use Eecrit.Web, :model
 
+  @default_string_max 50
+
   schema "users" do
     field :display_name, :string
     field :login_name, :string
@@ -9,5 +11,14 @@ defmodule Eecrit.User do
     field :organizational_scope, :string
 
     timestamps
+  end
+
+  def changeset(starting_value, additions \\ :invalid) do
+    starting_value
+    |> cast(additions, ~w(display_name login_name organizational_scope), [])
+    |> validate_length(:display_name, min: 1, max: @default_string_max)
+    |> validate_length(:login_name, min: 7, max: @default_string_max)
+    |> validate_length(:organizational_scope, min: 1, max: @default_string_max)
+    |> unique_constraint(:login_name)
   end
 end

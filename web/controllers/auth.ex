4fdfd23732191013d67fa1ffab52d3037010b132
Brian Marick <marick@exampler.com>
2016-07-09 15:12:15 -0500

    finish chapter 5

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -34,4 +34,8 @@ defmodule Eecrit.Auth do
         {:error, :not_found, conn}
     end
   end
+
+  def logout(conn) do
+    configure_session(conn, drop: true)
+  end
 end
--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -20,4 +20,10 @@ defmodule Eecrit.SessionController do
         |> render("new.html")
     end
   end
+
+  def delete(conn, _) do
+    conn
+    |> Auth.logout()
+    |> redirect(to: page_path(conn, :index))
+  end
 end

789e1198cb1ce94a0a27ca9a105a3f2e418467d0
Brian Marick <marick@exampler.com>
2016-07-11 19:34:18 -0500

    add some seed values for db

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,50 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Eecrit.Organization
+alias Eecrit.User
+alias Ecto.Changeset
+alias Eecrit.Repo
+
+defmodule U do
+  def fresh_start!() do
+  end
+
+  def add_user!(kwlist) do
+    %User{}
+    |> User.password_setting_changeset(as_map(kwlist))
+    |> Repo.insert!
+  end
+
+  def add_org!(kwlist) do
+    struct(Organization, as_map(kwlist))
+    |> Repo.insert!
+  end
+
+  defp as_map(kwlist), do: Enum.into(kwlist, %{})
+end
+
+
+# Organizations
+Repo.delete_all(Organization)
+U.add_org! short_name: "uiuc_aacup",
+           full_name: "University of Illinois Agricultural Animal Care and Use"
+
+# Users
+Repo.delete_all(User)
+U.add_user! display_name: "User 1", login_name: "1@y.com", password: "password"
+U.add_user! display_name: "User 2", login_name: "2@y.com", password: "password"
+
+
+# Relationships
+org = Repo.get_by(Organization, short_name: "uiuc_aacup") |> Repo.preload(:users)
+
+for u <- Repo.all(User) do
+  u
+  |> Repo.preload([:organizations, :current_organization])
+  |> Changeset.change
+  |> Changeset.put_assoc(:current_organization, org)
+  |> Changeset.put_assoc(:organizations, [org])
+  |> Repo.update!
+end    

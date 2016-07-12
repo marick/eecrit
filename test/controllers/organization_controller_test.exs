ea0d9946feddab36cd299db5ad430f50b6205ac2
Brian Marick <marick@exampler.com>
2016-07-12 15:12:37 -0500

    Skip original tests for now

--- test/controllers/organization_controller_test.exs
+++ test/controllers/organization_controller_test.exs
@@ -5,45 +5,53 @@ defmodule Eecrit.OrganizationControllerTest do
   @valid_attrs %{full_name: "some content"}
   @invalid_attrs %{}
 
+  @tag :skip
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, organization_path(conn, :index)
     assert html_response(conn, 200) =~ "Listing organizations"
   end
 
+  @tag :skip
   test "renders form for new resources", %{conn: conn} do
     conn = get conn, organization_path(conn, :new)
     assert html_response(conn, 200) =~ "New organization"
   end
 
+  @tag :skip
   test "creates resource and redirects when data is valid", %{conn: conn} do
     conn = post conn, organization_path(conn, :create), organization: @valid_attrs
     assert redirected_to(conn) == organization_path(conn, :index)
     assert Repo.get_by(Organization, @valid_attrs)
   end
 
+  @tag :skip
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
     conn = post conn, organization_path(conn, :create), organization: @invalid_attrs
     assert html_response(conn, 200) =~ "New organization"
   end
 
+  @tag :skip
   test "shows chosen resource", %{conn: conn} do
     organization = Repo.insert! %Organization{}
     conn = get conn, organization_path(conn, :show, organization)
     assert html_response(conn, 200) =~ "Show organization"
   end
 
+  @tag :skip
   test "renders page not found when id is nonexistent", %{conn: conn} do
     assert_error_sent 404, fn ->
       get conn, organization_path(conn, :show, -1)
     end
   end
 
+  @tag :skip
   test "renders form for editing chosen resource", %{conn: conn} do
     organization = Repo.insert! %Organization{}
     conn = get conn, organization_path(conn, :edit, organization)
     assert html_response(conn, 200) =~ "Edit organization"
   end
 
+  @tag :skip
   test "updates chosen resource and redirects when data is valid", %{conn: conn} do
     organization = Repo.insert! %Organization{}
     conn = put conn, organization_path(conn, :update, organization), organization: @valid_attrs
@@ -51,12 +59,14 @@ defmodule Eecrit.OrganizationControllerTest do
     assert Repo.get_by(Organization, @valid_attrs)
   end
 
+  @tag :skip
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
     organization = Repo.insert! %Organization{}
     conn = put conn, organization_path(conn, :update, organization), organization: @invalid_attrs
     assert html_response(conn, 200) =~ "Edit organization"
   end
 
+  @tag :skip
   test "deletes chosen resource", %{conn: conn} do
     organization = Repo.insert! %Organization{}
     conn = delete conn, organization_path(conn, :delete, organization)
--- test/models/organization_test.exs
+++ test/models/organization_test.exs
@@ -6,11 +6,13 @@ defmodule Eecrit.OrganizationTest do
   @valid_attrs %{full_name: "some content"}
   @invalid_attrs %{}
 
+  @tag :skip
   test "changeset with valid attributes" do
     changeset = Organization.changeset(%Organization{}, @valid_attrs)
     assert changeset.valid?
   end
 
+  @tag :skip
   test "changeset with invalid attributes" do
     changeset = Organization.changeset(%Organization{}, @invalid_attrs)
     refute changeset.valid?

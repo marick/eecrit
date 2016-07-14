52897210fd823591ad5a7d9a68d2df766e12a572
Brian Marick <marick@exampler.com>
2016-07-14 11:26:08 -0500

    Add test support

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -11,8 +11,15 @@ defmodule Eecrit.Auth do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
-    user = user_id && repo.get(User, user_id)
-    assign(conn, :current_user, user)
+
+    cond do
+      user = conn.assigns[:current_user] ->
+        conn # Allows tests to bypass authentication
+      user = user_id && repo.get(User, user_id) ->
+        assign(conn, :current_user, user)
+      true ->
+        assign(conn, :current_user, nil)
+    end
   end
 
   def authenticate_user(conn, _opts) do

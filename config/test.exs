0dd1270b7bf4bc3e7a0a714389123e9048d1b9ff
Brian Marick <marick@exampler.com>
2016-07-15 10:47:54 -0500

    Make encryption fast for tests

--- config/test.exs
+++ config/test.exs
@@ -17,3 +17,9 @@ config :eecrit, Eecrit.Repo,
   database: "eecrit_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+
+# Make encryption fast for tests
+config :comeonin,
+  bcrypt_log_rounds: 4,
+  pbkdf2_rounds: 1

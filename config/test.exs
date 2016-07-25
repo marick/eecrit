da82e360772faefacea3b01dccc612ffde865309
Brian Marick <marick@exampler.com>
2016-07-25 12:32:37 -0500

    Use travis-ci.com

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :eecrit, Eecrit.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  # password: "postgres",
   database: "eecrit_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox

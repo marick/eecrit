dda9ea3e7f22eb648e69b2f203f41af90cae6591
Brian Marick <marick@exampler.com>
2016-07-26 18:16:00 -0500

    Dead code

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,10 +1,4 @@
 defmodule Eecrit.UserView do
   use Eecrit.Web, :view
   alias Eecrit.User
-
-  def first_name(%User{display_name: name}) do
-    name
-    |> String.split(" ")
-    |> Enum.at(0)
-  end
 end

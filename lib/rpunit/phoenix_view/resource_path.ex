b0a17957db3530bd6977dead3accfcb8a0bc127e
Brian Marick <marick@exampler.com>
2016-08-21 15:09:28 -0500

    A better canonical format for resource paths

--- lib/rpunit/phoenix_view/resource_path.ex
+++ lib/rpunit/phoenix_view/resource_path.ex
@@ -5,12 +5,52 @@ defmodule RoundingPegs.ExUnit.PhoenixView.ResourcePath do
   """
   alias RoundingPegs.ExUnit.PhoenixState
 
+  @doc """
+  ## Examples
+
+      iex> animal_path(conn, :index) |> cast_to_path
+      "/animals"
+
+  """
   def cast_to_path(path) when is_binary(path), do: path
 
-  def cast_to_path(model, action, arglist, params) do
-    path_fn = PhoenixState.get(:path_fns) |> Map.get(model)
-    arglist = [PhoenixState.get(:endpoint), action] ++ arglist ++ [params]
-    apply(PhoenixState.get(:path_module), path_fn, arglist)
+  @doc """
+  ## Examples
+
+      iex> cast_to_path(:index, Animal)
+      "/animals"
+
+      iex> cast_to_path(:index, %Animal{...})
+      "/animals"
+
+      iex> cast_to_path(:show, [Animal, 1])
+      "/animals/1"
+
+      iex> cast_to_path(:show, [Animal, %Animal{id: 1, ...}])
+      "/animals/1"
+
+      iex> cast_to_path(:show, %Animal{id: 1, ...})
+      "/animals/1"
+
+      iex> cast_to_path(:delete, [:session_path, 3])
+      "/sessions/3"
+
+
+      %{model: OldAnimal, args: [], params: []}
+      %{fn: :animal_path, args: [], params: []}
+      
+  """
+
+  def cast_to_path(action, %{fn: fn_name} = descriptor) do
+    path_args = Map.get(descriptor, :args, [])
+    params = Map.get(descriptor, :params, [])
+    fn_args = [PhoenixState.get(:endpoint), action] ++ path_args ++ [params]
+    apply(PhoenixState.get(:path_module), fn_name, fn_args)
+  end
+
+  def cast_to_path(action, %{model: model} = descriptor) do
+    fn_name = PhoenixState.get(:path_fns) |> Map.get(model)
+    cast_to_path(action, Map.put(descriptor, :fn, fn_name))
   end
 end
   
--- test/rpunit/phoenix_view/resource_path_test.exs
+++ test/rpunit/phoenix_view/resource_path_test.exs
@@ -5,14 +5,97 @@ defmodule RoundingPegs.ExUnit.PhoenixView.ResourcePathTest do
   alias Eecrit.OldAnimal
   alias RoundingPegs.ExUnit.PhoenixView.ResourcePath
 
-  describe "cast_to_path" do 
-    test "A plain string is left unconverted" do
-      assert ResourcePath.cast_to_path("string") == "string"
-    end
-    
-    test "most expansive format" do
-      assert ResourcePath.cast_to_path(OldAnimal, :index, [], param1: 1, param2: "param2") ==
-        "/animals?param1=1&param2=param2"
-    end    
+  @subject &ResourcePath.cast_to_path/2
+  @path_fn :old_animal_path
+  @model Eecrit.OldAnimal
+
+  test "the fn-canonical format with args and params" do
+    input_with_arg = %{fn: @path_fn,
+                       args: [1],
+                       params: [param1: "p1", param2: 8]}
+
+    input_no_arg = %{fn: @path_fn,
+                     args: [],
+                     params: [param1: "p1"]}
+
+    # anchors
+    assert @subject.(:index, input_no_arg) == "/animals?param1=p1"
+    assert @subject.(:new, input_no_arg) == "/animals/new?param1=p1"
+    assert @subject.(:show, input_with_arg) == "/animals/1?param1=p1&param2=8"
+    assert @subject.(:edit, input_with_arg) == "/animals/1/edit?param1=p1&param2=8"
+
+    # forms
+    assert @subject.(:create, input_no_arg) == "/animals?param1=p1"
+    assert @subject.(:delete, input_with_arg) == "/animals/1?param1=p1&param2=8"
+    assert @subject.(:update, input_with_arg) == "/animals/1?param1=p1&param2=8"
   end
+
+  test "the fn-canonical format without params" do
+    input_with_arg = %{fn: @path_fn, args: [1], params: []}
+    input_no_arg = %{fn: @path_fn, args: [], params: []}
+
+    # anchors
+    assert @subject.(:index, input_no_arg) == "/animals"
+    assert @subject.(:new, input_no_arg) == "/animals/new"
+    assert @subject.(:show, input_with_arg) == "/animals/1"
+    assert @subject.(:edit, input_with_arg) == "/animals/1/edit"
+
+    # forms
+    assert @subject.(:create, input_no_arg) == "/animals"
+    assert @subject.(:delete, input_with_arg) == "/animals/1"
+    assert @subject.(:update, input_with_arg) == "/animals/1"
+  end
+
+  test "the model-canonical format" do
+    input_with_arg = %{model: @model, args: [1], params: [param1: "p1"]}
+    input_no_arg = %{model: @model, args: [], params: []}
+
+    assert @subject.(:index, input_no_arg) == "/animals"
+    assert @subject.(:show, input_with_arg) == "/animals/1?param1=p1"
+    assert @subject.(:create, input_no_arg) == "/animals"
+  end    
 end
+
+
+
+    # test "most expansive format - the different actions" do
+    #   # These are anchors.
+    #   # index
+    #   input = [OldAnimal, :index, [], [param1: 1, param2: "param2"]]
+    #   assert run(input) == "/animals?param1=1&param2=param2"
+
+    #   # show
+    #   input = [OldAnimal, :show, [1001], [param1: 1, param2: "param2"]]
+    #   assert run(input) == "/animals/1001?param1=1&param2=param2"
+
+    #   # new
+    #   input = [OldAnimal, :new, [], [param1: 1, param2: "param2"]]
+    #   assert run(input) == "/animals/new?param1=1&param2=param2"
+
+    #   # edit
+    #   input = [OldAnimal, :edit, [1001], [param1: 1, param2: "param2"]]
+    #   assert run(input) == "/animals/1001/edit?param1=1&param2=param2"
+
+    #   # These are forms - the paths are only a part of what needs checking
+    #   # create
+    #   input = [OldAnimal, :create, [], [param1: 1, param2: "param2"]]
+    #   assert run(input) == "/animals?param1=1&param2=param2"
+
+    #   # update
+    #   input = [OldAnimal, :update, [1001], [param1: 1, param2: "param2"]]
+    #   assert run(input) == "/animals/1001?param1=1&param2=param2"
+
+    #   # delete 
+    #   input = [OldAnimal, :delete, [1001], [param1: 1, param2: "param2"]]
+    #   assert run(input) == "/animals/1001?param1=1&param2=param2"
+    # end
+
+    # @tag :skip
+    # test "most expansive format - special-case values for args" do 
+    #   # instances can be used instead of specific path values
+    #   input = [OldAnimal, :show, [%OldAnimal{id: 1001}], [param1: 1, param2: "param2"]]
+    #   assert run(input) == "/animals/1001?param1=1&param2=param2"
+
+    #   # empty parameters
+    #   input = [OldAnimal, :edit, [%OldAnimal{id: 1001}], []]
+    #   assert run(input) == "/animals/1001/edit"

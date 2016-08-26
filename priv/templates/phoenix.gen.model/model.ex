49daace7e92c8d66f6e3fbba0dcc5355bc38068d
Brian Marick <marick@exampler.com>
2016-08-26 14:40:05 -0500

    My own model templates

--- priv/templates/phoenix.gen.model/model.ex
+++ priv/templates/phoenix.gen.model/model.ex
@@ -1,5 +1,8 @@
 defmodule <%= module %> do
   use <%= base %>.Web, :model
+  use <%= base %>.ModelDefaults, model: __MODULE__
+  resource_requires_ability :is_superuser  # You will want to change this.
+  alias <%= module %>
 
   schema <%= inspect plural %> do
 <%= for {k, _} <- attrs do %>    field <%= inspect k %>, <%= inspect types[k] %><%= schema_defaults[k] %>
@@ -8,13 +11,12 @@ defmodule <%= module %> do
     timestamps()
   end
 
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
-  def changeset(struct, params \\ %{}) do
+  @visible_fields []    # You will want to change this
+  @fields_always_required @visible_fields
+
+  defp changeset(struct, params) do
     struct
-    |> cast(params, [<%= Enum.map_join(attrs, ", ", &inspect(elem(&1, 0))) %>])
-    |> validate_required([<%= Enum.map_join(attrs, ", ", &inspect(elem(&1, 0))) %>])
-<%= for k <- uniques do %>    |> unique_constraint(<%= inspect k %>)
-<% end %>  end
+    |> cast(params, @visible_fields)
+    |> validate_required(@fields_always_required)
+  end
 end
--- priv/templates/phoenix.gen.model/model_test.exs
+++ priv/templates/phoenix.gen.model/model_test.exs
@@ -1,18 +1,45 @@
 defmodule <%= module %>Test do
   use <%= base %>.ModelCase
-
   alias <%= module %>
 
   @valid_attrs <%= inspect params %>
-  @invalid_attrs %{}
+  @empty_attrs %{}
 
-  test "changeset with valid attributes" do
-    changeset = <%= alias %>.changeset(%<%= alias %>{}, @valid_attrs)
-    assert changeset.valid?
+  test "a changeset for the `new` action" do
+    changeset = <%= alias %>.new_action_changeset
+    refute changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
-    changeset = <%= alias %>.changeset(%<%= alias %>{}, @invalid_attrs)
+  test "a changeset for the `create` action accepts valid attributes" do 
+    changeset = <%= alias %>.create_action_changeset(@valid_attrs)
+    assert changeset.valid?
+  end
+  
+  test "a changeset for the `create` action rejects missing attributes" do 
+    changeset = <%= alias %>.create_action_changeset(@empty_attrs)
     refute changeset.valid?
+  end
+
+  test "an edit changeset contains the original attributes" do
+    org = struct(<%= alias %>, @valid_attrs)
+    changeset = <%= alias %>.edit_action_changeset(org)
+    assert changeset.valid?
+    assert changeset.data == org
+  end
+
+  test "an update changeset processes update values" do
+    org = struct(<%= alias %>, @valid_attrs)
+    updates = %{fixme: "please"}  # You must add appropriate selected updates
+    changeset = <%= alias %>.update_action_changeset(org, updates)
+    assert changeset.valid?
+    assert changeset.changes == updates
+  end
+
+  test "an update changeset rejects bad values" do
+    org = struct(<%= alias %>, @valid_attrs)
+    invalids = %{fixme: "please"} # You must add appropriate invalid values
+    changeset = <%= alias %>.update_action_changeset(org, invalids)
+    refute changeset.valid?
+    assert Keyword.get(changeset.errors, :fixme)  # Fix this
   end
 end

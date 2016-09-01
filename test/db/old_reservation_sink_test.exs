eff3eee35992b9bde8482f41a958772b5bdf788b
Brian Marick <marick@exampler.com>
2016-09-01 17:28:38 -0500

    About putting a reservation into a database

---
+++ test/db/old_reservation_sink_test.exs
@@ -0,0 +1,30 @@
+defmodule Eecrit.OldReservationSinkTest do
+  use Eecrit.ModelCase
+  alias Eecrit.OldReservation
+  alias Eecrit.OldGroup
+  alias Eecrit.OldUse
+
+  test "complete_reservation" do
+    animal = insert_old_animal()
+    procedure = insert_old_procedure()
+    reservation_fields = %OldReservation{course: "vcm333",
+                                         first_date: Ecto.Date.cast!("2001-01-01"),
+                                         last_date: Ecto.Date.cast!("2012-12-12")}
+
+
+    OldReservation.insert_new!(reservation_fields, [animal], [procedure])
+
+    use = OldRepo.one(OldUse)
+    group = OldRepo.one(OldGroup)
+    reservation = OldRepo.one(OldReservation)
+
+    assert use.group_id == group.id
+    assert use.animal_id == animal.id
+    assert use.procedure_id == procedure.id
+
+    assert group.reservation_id == reservation.id
+
+    assert reservation.course == reservation_fields.course
+    assert reservation.first_date == reservation_fields.first_date
+  end
+end

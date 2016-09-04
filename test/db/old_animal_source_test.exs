03734c86b2c34d0ec4202a0c27d0641bbbd43d3e
Brian Marick <marick@exampler.com>
2016-09-04 17:26:12 -0500

    Adding more onto the base query.

--- test/db/old_animal_source_test.exs
+++ test/db/old_animal_source_test.exs
@@ -5,37 +5,51 @@ defmodule Eecrit.OldAnimalSourceTest do
   def run(options),
     do: OldAnimalSource.all_ordered(options) |> Enum.map(&Map.get(&1, :name))
   
-  def everything, do: run(include_out_of_service: true)
-  def in_service, do: run(include_out_of_service: false)
+  def everything_now, do: run(include_out_of_service: true)
+  def in_service_now, do: run(include_out_of_service: false)
 
-  describe "all_ordered" do 
-    test "returns human-style alphabetical ordering" do
-      insert_old_animal(name: "ab")
-      insert_old_animal(name: "AA")
-      insert_old_animal(name: "Z")
-      insert_old_animal(name: "m")
-      insert_old_animal(name: "12")
-
-      assert everything == ["12", "AA", "ab", "m", "Z"]
-
-      # With this data, no difference between two ways of calling
-      assert in_service == ["12", "AA", "ab", "m", "Z"]
-    end
-
-    test "does not return items that are out of service" do
+  test "returns human-style alphabetical ordering" do
+    insert_old_animal(name: "ab")
+    insert_old_animal(name: "AA")
+    insert_old_animal(name: "Z")
+    insert_old_animal(name: "m")
+    insert_old_animal(name: "12")
+    
+    assert everything_now == ["12", "AA", "ab", "m", "Z"]
+    
+    # With this data, no difference between two ways of calling
+    assert in_service_now == ["12", "AA", "ab", "m", "Z"]
+  end
+  
+  describe "time_based return values" do
+    setup do 
       yesterday = Timex.shift(Timex.today, days: -1) |> Ecto.Date.cast!
       today = Timex.today |> Ecto.Date.cast!
       tomorrow = Timex.shift(Timex.today, days: 1) |> Ecto.Date.cast!
-
+      insert_old_animal(name: "never", date_removed_from_service: nil)
       insert_old_animal(name: "yesterday", date_removed_from_service: yesterday)
       insert_old_animal(name: "today", date_removed_from_service: today)
       insert_old_animal(name: "tomorrow", date_removed_from_service: tomorrow)
 
-      # Note that animals are removed from service at the start of the day
-      assert in_service == ["tomorrow"]
+      provides([yesterday, today, tomorrow])
+    end
 
-      # But everything can be returned
-      assert everything == ["today", "tomorrow", "yesterday"]
-    end      
+    test "does not return items that are out of service at the current moment" do
+      # Note that animals are removed from service at the start of the day
+      assert in_service_now == ["never", "tomorrow"]
+
+      # But everything_now can be returned
+      assert everything_now == ["never", "today", "tomorrow", "yesterday"]
+    end
+
+    test "service overlap", c do
+      assert run(date_range: {c.yesterday, c.tomorrow}) ==
+        ["never", "today", "tomorrow", "yesterday"]
+      # Anything available for part of the range is returned.
+      assert run(date_range: {c.today, c.tomorrow}) ==
+        ["never", "today", "tomorrow"]
+      assert run(date_range: {c.yesterday, c.today}) ==
+        ["never", "today", "tomorrow", "yesterday"]
+    end
   end
 end
--- web/db/old_animal_source.ex
+++ web/db/old_animal_source.ex
@@ -4,22 +4,37 @@ defmodule Eecrit.OldAnimalSource do
 
   @repo Eecrit.OldRepo
 
+  defmodule P do 
+    def tailor(base_query, options) when is_list(options) do 
+      Enum.reduce(options, base_query, fn(option, query_so_far) ->
+        tailor(query_so_far, option)
+      end)
+    end
+    
+    def tailor(query, {:include_out_of_service, false}) do
+      query
+      |> where([a],
+         is_nil(a.date_removed_from_service) or
+         a.date_removed_from_service > fragment("CURRENT_DATE"))
+    end
+
+    def tailor(query, {:date_range, {first_date, _}}) do
+      query
+      |> where([a],
+         is_nil(a.date_removed_from_service) or
+         a.date_removed_from_service >= ^Ecto.Date.cast!(first_date))
+    end
+    
+    def tailor(query, _), do: query
+  end
+
+  ## PUBLIC
+
   def all_ordered(options \\ []) do
-    base = from a in OldAnimal, order_by: fragment("lower(?)", a.name)
-    tailored = add_tailoring(base, options)
-    @repo.all(tailored)
-  end
+    query = from a in OldAnimal, order_by: fragment("lower(?)", a.name)
 
-  defp add_tailoring(base_query, options) do 
-    Enum.reduce(options, base_query, fn(option, query_so_far) ->
-      tailor(query_so_far, option)
-    end)
-  end
-  
-  defp tailor(query, {:include_out_of_service, false}) do
     query
-    |> where([a], is_nil(a.date_removed_from_service) or a.date_removed_from_service > fragment("CURRENT_DATE"))
+    |> P.tailor(options)
+    |> @repo.all
   end
-
-  defp tailor(query, _), do: query
 end

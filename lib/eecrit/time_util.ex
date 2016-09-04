02c701689d5fa6519be8e877dd26ec0f2c1b6afc
Brian Marick <marick@exampler.com>
2016-09-04 13:39:16 -0500

    More work on time utilities

--- lib/eecrit/time_util.ex
+++ lib/eecrit/time_util.ex
@@ -1,16 +1,43 @@
 defmodule Eecrit.TimeUtil do
   use Timex
 
-  def ecto_date_to_date(ecto_date) do
-    ecto_date
-    |> Ecto.Date.to_erl()
-    |> Date.from_erl!()
+  # TODO: Use Timex compatibility with Ecto.Date
+  # TODO: protocols
+
+  def cast_to_date!(d = %Date{}), do: d
+  def cast_to_date!(d) when is_tuple(d), do: Date.from_erl!(d)
+  def cast_to_date!(d = %Ecto.Date{}), do: cast_to_date!(Ecto.Date.to_erl(d))
+  def cast_to_date!(d) when is_binary(d), do: Date.from_iso8601!(d)
+
+  def cast_to_erl!(d), do: d |> cast_to_date! |> Date.to_erl
+
+  def friendly_format(nil), do: ""
+  def friendly_format(date),
+    do: date |> cast_to_date! |> Timex.format!("%B %-d, %Y", :strftime)
+
+  def adjust_range({first, last}, within: {earliest_first, latest_last}) do
+    use_first =
+      if cast_to_erl!(first) < cast_to_erl!(earliest_first),
+        do: earliest_first, else: first
+
+    use_last =
+      if cast_to_erl!(last) > cast_to_erl!(latest_last),
+        do: latest_last, else: last
+
+    {use_first, use_last}
   end
 
-  def format_ecto_date(nil), do: ""
-  def format_ecto_date(ecto_date) do 
-    ecto_date
-    |> ecto_date_to_date
-    |> Timex.format!("%B %-d, %Y", :strftime)
+  def adjust_range_in_struct(struct, {first_key, last_key}, within: bounds) do
+    {first, last} =
+      {Map.get(struct, first_key), Map.get(struct, last_key)}
+      |> adjust_range(within: bounds)
+
+    struct
+    |> Map.put(first_key, first)
+    |> Map.put(last_key, last)
+  end
+
+  def days_covered({first, last}) do
+    Timex.diff(cast_to_date!(last), cast_to_date!(first), :days) + 1
   end
 end
---
+++ test/lib/eecrit/time_util_test.exs
@@ -0,0 +1,79 @@
+defmodule Eecrit.TimeUtilTest do
+  defstruct first: nil, last: nil
+  alias Eecrit.TimeUtil, as: S
+  use ExUnit.Case
+
+  test "cast_to_date!" do
+    expected = ~D[2012-01-23]
+    assert S.cast_to_date!(expected) == expected
+    assert S.cast_to_date!({2012, 1, 23}) == expected
+    assert S.cast_to_date!(Ecto.Date.cast!("2012-01-23")) == expected
+    assert S.cast_to_date!("2012-01-23") == expected
+  end
+
+  test "cast_to_erl!" do
+    expected = {2012, 1, 23}
+    assert S.cast_to_erl!(expected) == expected
+    assert S.cast_to_erl!(~D[2012-01-23]) == expected
+    assert S.cast_to_erl!(Ecto.Date.cast!("2012-01-23")) == expected
+    assert S.cast_to_erl!("2012-01-23") == expected
+  end
+
+  test "friendly-format" do
+    assert S.friendly_format(nil) == ""
+
+    expected = "January 23, 2012"
+    assert S.friendly_format(~D[2012-01-23]) == expected
+    assert S.friendly_format({2012, 1, 23}) == expected
+    assert S.friendly_format(Ecto.Date.cast!("2012-01-23")) == expected
+  end
+
+  test "adjust_range" do
+    nine = ~D[2012-01-09]
+    ten = ~D[2012-01-10]
+    eleven = ~D[2012-01-11]
+    nineteen = ~D[2012-01-19]
+    twenty = ~D[2012-01-20]
+    twentyone = ~D[2012-01-21]
+
+    bounds = {ten, twenty}
+    assert S.adjust_range(bounds, within: bounds) == bounds
+    assert S.adjust_range({nine, twentyone}, within: bounds) == bounds
+    assert S.adjust_range({eleven, nineteen}, within: bounds) == {eleven, nineteen}
+
+    assert S.adjust_range({nine, eleven}, within: bounds) == {ten, eleven}
+    assert S.adjust_range({nineteen, twentyone}, within: bounds) == {nineteen, twenty}
+
+    # Works with other formats
+    nine = Ecto.Date.cast!(nine)
+    ten = Ecto.Date.cast!(ten)
+    twenty = Ecto.Date.cast!(twenty)
+    twentyone = Ecto.Date.cast!(twentyone)
+    bounds = {ten, twenty}
+    assert S.adjust_range({nine, twentyone}, within: bounds) == bounds
+  end
+
+  test "adjust_range_in_struct" do
+    nine = ~D[2012-01-09]
+    ten = ~D[2012-01-10]
+    twenty = ~D[2012-01-20]
+    twentyone = ~D[2012-01-21]
+
+    bounds = {ten, twenty}
+    source = %__MODULE__{first: nine, last: twentyone}
+    expected = %__MODULE__{first: ten, last: twenty}
+    
+    assert S.adjust_range_in_struct(source, {:first, :last}, within: bounds) ==
+      expected
+  end
+
+  test "days_covered" do
+    day = ~D[2012-12-30]
+    next_day = {2012, 12, 31}
+    even_later = Ecto.Date.cast!("2013-01-01")
+
+    assert S.days_covered({day, day}) == 1
+    assert S.days_covered({day, next_day}) == 2
+    assert S.days_covered({day, even_later}) == 3
+  end
+end

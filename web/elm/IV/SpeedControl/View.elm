758e0d384e18c8796e60d32c36ff241b42f5ab26
Brian Marick <marick@exampler.com>
2016-09-23 16:06:31 -0500

    Push view down to SpeedControl

--- web/elm/IV/SpeedControl/View.elm
+++ web/elm/IV/SpeedControl/View.elm
@@ -1,3 +1,22 @@
 module IV.SpeedControl.View exposing (view)
 
-view model = []
+import Html exposing (..)
+import Html.Attributes as Attr
+import IV.SpeedControl.Model exposing (Model)
+import IV.Msg as TopMsg
+import IV.SpeedControl.Msg as Msg
+import Html.Events as Events
+
+changeHandler : String -> TopMsg.Msg
+changeHandler string =
+  TopMsg.ToSpeedControl (Msg.ChangedTextField string)
+
+view : Model -> Html TopMsg.Msg
+view model =
+  div
+    []
+    [ input [ Attr.value model.string, Events.onInput changeHandler] []
+    , button [Events.onClick TopMsg.ChangeDripRate ] [ text "Go" ]
+    ]
+
+  
--- web/elm/IV/View.elm
+++ web/elm/IV/View.elm
@@ -1,27 +1,18 @@
 module IV.View exposing (view)
 
-import IV.Msg as TopMsg
+import IV.Msg exposing (Msg)
 import IV.Model exposing (Model)
 import Html exposing (..)
 import Html.Attributes as Attr
 import IV.Backdrop exposing (provideBackdropFor)
 import IV.Droplet.View as Droplet
-import Html.Events as Events
-import IV.SpeedControl.Msg exposing (Msg(ChangedTextField))
+import IV.SpeedControl.View as SpeedControl
 
-changeHandler : String -> TopMsg.Msg
-changeHandler string =
-  TopMsg.ToSpeedControl (ChangedTextField string)
-                
-
-view : Model -> Html TopMsg.Msg
+view : Model -> Html Msg
 view model =
     div
-      [
-       Attr.style [ ( "margin", "0px auto" ), ( "width", "500px" ), ( "height", "500px" ), ( "cursor", "pointer" ) ]
+      [ Attr.style [ ( "margin", "0px auto" ), ( "width", "500px" ), ( "height", "500px" ), ( "cursor", "pointer" ) ]
       ]
-      [
-        provideBackdropFor [(Droplet.render model.droplet)]
-      , input [ Attr.value model.speedControl.string, Events.onInput changeHandler] []
-      , button [Events.onClick TopMsg.ChangeDripRate ] [ text "Go" ]
+      [ provideBackdropFor [(Droplet.render model.droplet)]
+      , SpeedControl.view model.speedControl
       ]

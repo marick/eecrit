b74ebc86fa76d366b8966a2d0fc0ea7e90f9bc2e
Brian Marick <marick@exampler.com>
2016-11-04 14:47:49 -0500

    == deployed version v468

--- web/elm/IV/Version.elm
+++ web/elm/IV/Version.elm
@@ -1,9 +1,9 @@
 
--- Created at Fri Nov  4 14:43:22 CDT 2016 by bin/deploy
+-- Created at Fri Nov  4 14:47:41 CDT 2016 by bin/deploy
 
 module IV.Version exposing (text, source)
 
-text = "Version  of 2016-Nov-04"
+text = "Version v468 of 2016-Nov-04"
 
 source = "https://github.com/marick/eecrit/tree/master/web/elm/IV"
 

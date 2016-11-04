7a2601d2895f4b7ac4dd625939defce3fd5a1405
Brian Marick <marick@exampler.com>
2016-11-04 14:43:30 -0500

    == deployed version X

--- web/elm/IV/Version.elm
+++ web/elm/IV/Version.elm
@@ -1,5 +1,9 @@
 
-module IV.Version exposing (text)
+-- Created at Fri Nov  4 14:43:22 CDT 2016 by bin/deploy
 
-text = "Version 466 of 2016-Nov-04"
+module IV.Version exposing (text, source)
+
+text = "Version  of 2016-Nov-04"
+
+source = "https://github.com/marick/eecrit/tree/master/web/elm/IV"
 

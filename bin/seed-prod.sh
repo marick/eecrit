33bd5d7858a0e1a9b15dbb0687fa166dad5203a1
Brian Marick <marick@exampler.com>
2016-11-17 12:31:22 -0600

    Drop pool size down AGAIN

--- bin/seed-prod.sh
+++ bin/seed-prod.sh
@@ -1,9 +1,18 @@
+echo "This modifies ***PRODUCTION***. Do you want to continue?"
+read -n 1 response
+if [ "$response" != "y" ]; then
+    exit 1
+fi
+
+
+
+
 export MIX_ENV=prod
 set -x
 
 export DATABASE_URL=`heroku config:get DATABASE_URL`
 export OLD_DATABASE_URL=`heroku config:get DATABASE_URL --app critter4us`
-export POOL_SIZE=3
+export POOL_SIZE=1
 
 mix compile
 mix ecto.migrate

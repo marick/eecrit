e0a0503b0ea1f0f1a6345934fb78b013006a3343
Brian Marick <marick@exampler.com>
2016-11-18 10:54:34 -0600

    Remove old video-controller practice code

--- web/controllers/help_controller.ex
+++ web/controllers/help_controller.ex
@@ -2,6 +2,6 @@ defmodule Eecrit.HelpController do
   use Eecrit.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html", video: %{id: "irrelevant"}
+    render conn, "index.html"
   end
 end
--- web/views/help_view.ex
+++ web/views/help_view.ex
@@ -1,5 +1,3 @@
 defmodule Eecrit.HelpView do
   use Eecrit.Web, :view
-
-  def player_id(_video), do: "U7p_E6Bq2bQ"
 end

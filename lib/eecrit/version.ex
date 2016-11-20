ded359daed29af25dcafe21fbdd82cfe7952f1d2
Brian Marick <marick@exampler.com>
2016-11-19 18:11:52 -0600

    == deployed version v539

--- lib/eecrit/version.ex
+++ lib/eecrit/version.ex
@@ -2,7 +2,7 @@
 defmodule Eecrit.Version do
 
   def version do
-    "Version v523, made public on 2016-Nov-18"
+    "Version v539, made public on 2016-Nov-19"
   end
 end
 
--- web/elm/IV/Version.elm
+++ web/elm/IV/Version.elm
@@ -1,9 +1,9 @@
 
--- Created at Sat Nov 19 17:00:58 CST 2016 by bin/deploy
+-- Created at Sat Nov 19 18:11:43 CST 2016 by bin/deploy
 
 module IV.Version exposing (text, source)
 
-text = "Version v536 of 2016-Nov-19"
+text = "Version v539 of 2016-Nov-19"
 
-source = "https://github.com/marick/eecrit/tree/v536/web/elm/IV"
+source = "https://github.com/marick/eecrit/tree/v539/web/elm/IV"
 

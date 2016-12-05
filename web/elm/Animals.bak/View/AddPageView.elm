40a7c0092ab5a66134432ec5bbb435782c8dbe8d
Brian Marick <marick@exampler.com>
2016-12-05 14:35:56 -0600

    Rename old files to keep from getting confused in Emacs

--- web/elm/Animals.bak/View/AddPageView.elm
+++
@@ -1,8 +0,0 @@
-module Animals.View.AddPageView exposing (..)
-
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Pile.Bulma as Bulma
-
-view model =
-  Bulma.infoMessage "Can't Add Animals Yet" "The page hasn't been written yet."

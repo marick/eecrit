ee4c72467c181f82ef9051b7270b561591a89eab
Brian Marick <marick@exampler.com>
2017-01-06 18:58:19 -0600

    Made a TODO for something to change later

--- web/elm/Animals/OutsideWorld/H.elm
+++ web/elm/Animals/OutsideWorld/H.elm
@@ -2,6 +2,11 @@ module Animals.OutsideWorld.H exposing (..)
 
 import Animals.Animal.Types exposing (..)
 
+-- TODO: This is a pointless indirection. Rather than have
+-- pass `AnimalGotSaved (AnimalSaveResults 5)`, just pass
+-- pass `AnimalGotSaved 5`.
+-- Existing form is a hangover from previous mechanism.
+
 type AnimalSaveResults
   = AnimalUpdated Id 
 

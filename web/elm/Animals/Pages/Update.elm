621466af5e4c5f46595c903b28bd3cca91995586
Brian Marick <marick@exampler.com>
2017-01-12 18:04:45 -0600

    Something weird with lenses

--- web/elm/Animals/Pages/Update.elm
+++ web/elm/Animals/Pages/Update.elm
@@ -1,6 +1,6 @@
 module Animals.Pages.Update exposing (update)
 
-import Animals.Model exposing (..)
+import Animals.Model as Model exposing (..)
 import Animals.Msg exposing (..)
 import Animals.Pages.Navigation as Page
 
@@ -10,7 +10,7 @@ update : PageOperation -> Model -> ( Model, Cmd Msg )
 update op model =
   case op of 
     NoticeChange location ->
-      model |> model_page.set (Page.fromLocation location) |> noCmd
+      model |> Model.model_page.set (Page.fromLocation location) |> noCmd
       
     StartChange page ->
       ( model, Page.toPageChangeCmd page )

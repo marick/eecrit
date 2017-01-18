9c94adaa161c45c240a1515861ae80fdc8fb33f5
Brian Marick <marick@exampler.com>
2017-01-18 12:03:35 -0600

    Turn a model into a (model, cmd)

--- web/elm/Pile/UpdateHelpers.elm
+++ web/elm/Pile/UpdateHelpers.elm
@@ -6,6 +6,12 @@ Typically used at the end of a |> pipeline that transforms a model.
 noCmd : t -> (t, Cmd msg)
 noCmd model = model ! []
 
+{-| Given a command and a model, create a `(model, cmd)`.
+-}
+addCmd : Cmd msg -> model -> (model, Cmd msg)
+addCmd cmd model =
+ (model, cmd)
+
 {-| Given a pair `(model, stuff)`, create a `(model, Cmd Msg)` by applying
 the given function to the `stuff`. 
 -}
